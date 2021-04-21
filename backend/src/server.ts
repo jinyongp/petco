7838f6a6e114937f6e1b106cda295dd70f77b73d
jinyongp <dev.jinyongp@gmail.com>
2021-04-22 01:34:50 +0900

    Add apollo-server-express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,16 @@
 require("dotenv").config();
-import { ApolloServer } from "apollo-server";
+import morgan from "morgan";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 import schema from "./schema";
 
 const server = new ApolloServer({ schema });
 const { PORT } = process.env;
 if (!PORT) throw new Error(".env file is not set");
 
-server.listen(PORT).then(({ url }) => console.log(`Running server on ${url}`));
+const app = express();
+app.use(morgan("tiny"));
+server.applyMiddleware({ app });
+app.listen({ port: PORT }, () => {
+  console.log(`✅ Running server on http://localhost:${PORT}/graphql`);
+});
