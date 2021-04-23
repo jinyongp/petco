d661dade59a5be9d24f062953c9843fc47519994
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-04-23 22:59:43 +0900

    Add Web home

---
+++ frontend/web/src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="hello">
+    <div>{{ msg }}</div>
+    <hr />
+    <hr />
+    <v-text-field></v-text-field>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
