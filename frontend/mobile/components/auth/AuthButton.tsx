286176bb50d0744363a2d8c779046fcd1ac0c96a
jinyongp <dev.jinyongp@gmail.com>
2021-04-23 22:44:13 +0900

    Add AuthButton

---
+++ frontend/mobile/components/auth/AuthButton.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Button = styled.TouchableOpacity`
+  background-color: #fec544;
+  width: 100%;
+  padding: 20px 20px;
+  border-radius: 30px;
+  opacity: ${({ disabled }) => (disabled ? "0.5" : "1")};
+`;
+
+const InnerText = styled.Text`
+  font-size: 15px;
+  font-weight: 800;
+  text-align: center;
+`;
+
+export default function AuthButton({ onPress, text, disabled }) {
+  return (
+    <Button onPress={onPress} disabled={disabled} accessible={!disabled}>
+      <InnerText>{text}</InnerText>
+    </Button>
+  );
+}
+
+AuthButton.defaultProps = {
+  disabled: false,
+};
--- frontend/mobile/components/auth/index.ts
+++ frontend/mobile/components/auth/index.ts
@@ -1,2 +1,3 @@
 export { default as AuthLayout } from "./AuthLayout";
+export { default as AuthButton } from "./AuthButton";
 export { default as TextInputIcon } from "./TextInputIcon";
