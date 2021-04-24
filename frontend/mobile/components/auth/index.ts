d95a7bab45b9bf092bcccdcc1262ce82a691b7e5
jinyongp <dev.jinyongp@gmail.com>
2021-04-24 21:01:54 +0900

    Add TextInputLabel

--- frontend/mobile/components/auth/index.ts
+++ frontend/mobile/components/auth/index.ts
@@ -2,3 +2,4 @@ export { default as AuthLayout } from "./AuthLayout";
 export { default as AuthButton } from "./AuthButton";
 export { default as AuthLink } from "./AuthLink";
 export { default as TextInputIcon } from "./TextInputIcon";
+export { default as TextInputLabel } from "./TextInputLabel";
