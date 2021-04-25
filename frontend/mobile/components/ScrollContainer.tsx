73420182f2d629829b678b58b0dfda26e8d9b58d
jinyongp <dev.jinyongp@gmail.com>
2021-04-26 00:01:51 +0900

    Add ScrollContainer

---
+++ frontend/mobile/components/ScrollContainer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { ScrollView } from "react-native";
+
+export default function ScreenLayout({ children }) {
+  return (
+    <ScrollView
+      style={{ width: "100%", flex: 1 }}
+      contentContainerStyle={{ flexGrow: 1, justifyContent: "center" }}
+    >
+      {children}
+    </ScrollView>
+  );
+}
