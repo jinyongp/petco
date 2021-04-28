baedb9e0130f6fbc2c1b68c2f845220b6a183e54
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-04-28 17:35:47 +0900

    Update SignIn

--- frontend/web/src/components/SignIn.vue
+++ frontend/web/src/components/SignIn.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <input type="text" />
+    <div class="textLogIn">LOGIN</div>
     <br />
-    <input type="password" />
+    <div class="text username">username</div>
+    <input type="text" class="inputUser" />
     <br />
-    <button @click="loginAlert">로그인</button>
+    <div class="text password">password</div>
+    <input type="password" class="inputPassword" />
+    <br />
+    <button class="logInButton" @click="loginAlert">로그인 하기</button>
   </div>
 </template>
 
@@ -14,8 +18,100 @@ export default Vue.extend({
   methods: {
     loginAlert() {
       alert("펫코에 오신 것을 환영합니다");
-      this.$router.push("/hospital");
+      this.$router.push("/appointment");
     },
   },
 });
 </script>
+
+<style scoped>
+.textLogIn {
+  position: absolute;
+  width: 194px;
+  height: 63px;
+  left: 900px;
+  top: 230px;
+
+  font-family: NanumGothic;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 64px;
+  line-height: 63px;
+  letter-spacing: -0.3px;
+
+  color: #000000;
+}
+.text {
+  /* 메인 카피 제목 */
+  font-family: NanumGothic;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 26px;
+  line-height: 26px;
+  /* identical to box height */
+  letter-spacing: -0.3px;
+
+  color: #000000;
+}
+.username {
+  position: absolute;
+  width: 120px;
+  height: 26px;
+  left: 897px;
+  top: 377px;
+}
+.password {
+  /* Passward */
+
+  position: absolute;
+  width: 111px;
+  height: 26px;
+  left: 897px;
+  top: 540px;
+}
+.inputUser {
+  position: absolute;
+  width: 200px;
+  height: 45px;
+  left: 888px;
+  top: 440px;
+
+  background: #ffffff;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 30px;
+}
+.inputPassword {
+  position: absolute;
+  width: 200px;
+  height: 45px;
+  left: 888px;
+  top: 600px;
+
+  background: #ffffff;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 30px;
+}
+
+.logInButton {
+  position: absolute;
+  width: 200px;
+  height: 45px;
+  left: 888px;
+  top: 720px;
+
+  background: #fec544;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.25);
+  border-radius: 30px;
+
+  /* 메인 카피 제목 */
+  font-family: NanumGothic;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 22px;
+  line-height: 22px;
+  /* identical to box height */
+  letter-spacing: -0.3px;
+
+  color: #000000;
+}
+</style>
