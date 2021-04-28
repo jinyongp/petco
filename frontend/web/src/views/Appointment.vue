e81fcf728edeffddea631c6d2e19746a246d02cb
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-04-28 17:38:03 +0900

    Add appoinment

---
+++ frontend/web/src/views/Appointment.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  data() {
+    return {
+      msg: "펫코 예약 시스템입니다",
+    };
+  },
+});
+</script>
