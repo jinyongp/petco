d1ba8cce20253c143d3e6acb4cf97cddb525d285
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-05-03 03:44:26 +0900

    Update Home

--- frontend/web/src/views/Home.vue
+++ frontend/web/src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="hello">
     <img class="img" src="../assets/Flying_iPhone_X_Mockups.png" />
-    <SignIn />
+    <div v-if="loginDone">{{ name }} 환영합니다 :)</div>
+    <div if-else>
+      <SignIn />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import SignIn from "../components/SignIn.vue";
+// import gql from "graphql-tag";
 
 @Component({
   components: {
@@ -16,6 +20,17 @@ import SignIn from "../components/SignIn.vue";
 })
 export default class Home extends Vue {
   @Prop() private msg!: string;
+  loginDone: boolean = false;
+  name: string = "";
+  // apollo: {
+  //   vets: gql` query{
+  //     vets{
+  //
+  //       name
+  //
+  //     }
+  //   }`,
+  // },
 }
 </script>
 
