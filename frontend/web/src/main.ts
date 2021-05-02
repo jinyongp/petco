d8a1b1fd39660a539561b38d766ea96184704d0d
정근석 <jeong-geunseok@jeong-geunseog-ui-MacBookPro.local>
2021-05-03 03:39:17 +0900

    Update main.ts

--- frontend/web/src/main.ts
+++ frontend/web/src/main.ts
@@ -1,10 +1,18 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import VueApollo from "vue-apollo";
+import apolloClient from "./apollo";
+// import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false;
 
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+});
+
 new Vue({
   router,
+  apolloProvider,
   render: (h) => h(App),
 }).$mount("#app");
