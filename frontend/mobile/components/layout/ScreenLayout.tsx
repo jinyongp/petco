669b21f0fe6b7efd759d62431c798cfb095bc4d3
jinyongp <dev.jinyongp@gmail.com>
2021-05-04 01:31:59 +0900

    Update ScreenLayout

--- frontend/mobile/components/layout/ScreenLayout.tsx
+++ frontend/mobile/components/layout/ScreenLayout.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
-import Container from "../Container";
-import DismissKeyboard from "./DismissKeyboard";
 import PropTypes from "prop-types";
 import { ScreenLayoutProps } from "../@types";
 
@@ -11,12 +9,20 @@ export default function ScreenLayout({
 }: ScreenLayoutProps): JSX.Element {
   return (
     <KeyboardAwareScrollView
-      style={{ backgroundColor: "white" }}
-      contentContainerStyle={{ flex: 1 }}
+      style={{
+        backgroundColor: "white",
+      }}
+      keyboardDismissMode="interactive"
+      contentContainerStyle={{
+        flexGrow: 1,
+        justifyContent: "center",
+        alignItems: align,
+        paddingHorizontal: "7%",
+        paddingVertical: "5%",
+      }}
+      extraScrollHeight={50}
     >
-      <Container padding="8%" style={{ alignItems: align, flex: 1 }}>
-        {children}
-      </Container>
+      {children}
     </KeyboardAwareScrollView>
   );
 }
