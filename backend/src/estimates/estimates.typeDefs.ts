0b9322d3089b776fed91bf7a1aa9b5516e0242a8
Ukhwan <dladnrghks22@gmail.com>
2021-05-03 18:11:12 +0900

    add a mutation requestEstimate

--- backend/src/estimates/estimates.typeDefs.ts
+++ backend/src/estimates/estimates.typeDefs.ts
@@ -2,7 +2,14 @@ import { gql } from "apollo-server";
 
 export default gql`
   type Estimate {
-    id: Int!
     # TODO - type definitions
+    id: Int
+    request_comment: String
+    status: Status
+    vet_id:Int
+    user_id:Int
+    pet_id:Int
+    created_at:String
+    updated_at:String
   }
 `;
