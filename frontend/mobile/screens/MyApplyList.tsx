e71b9c03fa1e8e765e2c545c60265e8c21783e2c
jinyongp <dev.jinyongp@gmail.com>
2021-05-05 05:21:48 +0900

    Add MyApplyList screen

--- frontend/mobile/screens/MyApplyList.tsx
+++ frontend/mobile/screens/MyApplyList.tsx
@@ -1,12 +1,34 @@
 import React from "react";
-import { Text } from "react-native";
-import { Container, ScreenLayout } from "../components";
+import Estimate from "../assets/icons/estimate.svg";
+import Stethoscope from "../assets/icons/stethoscope.svg";
+import {
+  Container,
+  NanumText,
+  ScreenLayout,
+  TouchableContainer,
+} from "../components";
 
 export default function MyProfile() {
   return (
-    <ScreenLayout>
+    <ScreenLayout align="flex-start">
+      <NanumText type="header" margin={{ top: 50, bottom: 57 }}>
+        {`서강준님의${"\n"}신청 내역`}
+      </NanumText>
+      <Container margin={{ bottom: 40 }}>
+        <TouchableContainer row height={75}>
+          <Estimate width={33} height={45} />
+          <NanumText position="center" type="button" margin={{ left: 25 }}>
+            견적 신청 내역
+          </NanumText>
+        </TouchableContainer>
+      </Container>
       <Container>
-        <Text>MyApplyList</Text>
+        <TouchableContainer row height={75}>
+          <Stethoscope width={33} height={40} />
+          <NanumText position="center" type="button" margin={{ left: 25 }}>
+            예약 신청 내역
+          </NanumText>
+        </TouchableContainer>
       </Container>
     </ScreenLayout>
   );
