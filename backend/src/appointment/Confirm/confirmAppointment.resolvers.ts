9446e8ba679d5b85ac1d89c7d864b2a54b8c3527
Ukhwan <dladnrghks22@gmail.com>
2021-05-12 20:55:30 +0900

    re-build a mutation confirmAppointment

--- backend/src/appointment/Confirm/confirmAppointment.resolvers.ts
+++ backend/src/appointment/Confirm/confirmAppointment.resolvers.ts
@@ -1,9 +1,25 @@
 import {Resolvers} from "../../types"
-
+import {AppointmentsPayLoadTypes} from "../appointments.types";
 
 const resolvers:Resolvers = {
   Mutation:{
-    confirmAppointment: async(_,where,client):Promise<any> =>{
+    confirmAppointment: async(_,where,{client,currentVets}):Promise<AppointmentsPayLoadTypes> =>{
+      if(!currentVets) return { ok:false, status:404 }
+
+      try{
+
+        const appointments = await client.appointments.update({
+          data:{ status:"confirm" },
+          where
+        });
+        return appointments ? { ok:true, appointments } : { ok:false, status:404 };
+
+      }catch(e){
+
+        console.log(e);
+        return { ok:false, status:500 };
+
+      }
       // const appointment = await client.appointments.update({data:{status:"confirm"},where})
       // .catch(()=>{return null})
       // if(!appointment) return { status:404, message:"예약확정에 실패하였습니다."}
